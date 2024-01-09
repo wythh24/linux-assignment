5f438c7b546d34e6940a15890f90c1295349be53
Mad_ <doos4230@gmail.com>
2024-01-10 00:10:21 +0700

    remove unused file

--- MyInfo.sh
+++ MyInfo.sh
@@ -1,6 +1,46 @@
 # !/bin/bash
 
-source usages.sh
+# Linux assignment semester 1
+# Teacher: Vidol Loeung
+
+# Team: 1111
+
+# Members
+# Davit
+# Lita
+# Leang eng
+# Siyat
+# Daray
+# Lao Chheng
+
+# Output usage of commands and options
+usage(){
+    command=$1
+    echo Usage: $command "[OPTION]"... OBJECT...
+}
+
+user_cmd(){
+    echo "Example: $0 u <username>"
+    echo "Options: u | U | user | USER"
+}
+
+if [ "$#" -lt 2 ]; then
+
+    case "$1" in
+        u | U | user | USER)
+            user_cmd $1
+            exit 1
+            ;;
+        h | H | host | HOST)
+            echo "Usage : $0 h <hostname>"
+            exit 1
+            ;;
+        p | P | path | PATH)
+            echo "Usage : $0 p <pathname>"
+            exit 1
+            ;;
+    esac
+fi
 
 # User infor
 user_info(){
